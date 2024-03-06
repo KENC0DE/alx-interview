cf652f72f4009469dd6d95e6cec1399878110281
KENC0DE <kenansameseret@gmail.com>
2024-03-07 01:50:36 +0530

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,12 +3,13 @@
 Pascal Triangle
 """
 
+
 def pascal_triangle(n):
+    """ Returns Pascal Triangle list """
     if n <= 0:
         return []
 
     trig = []
-    
     for i in range(n):
         if i == 0:
             row = [i]
@@ -19,8 +20,8 @@ def pascal_triangle(n):
             priv_row = row
 
         if i > 1:
-            new_row = [priv_row[i] + priv_row[i + 1] for i in range(len(priv_row))
-                       if i + 1 < len(priv_row)]
+            new_row = [priv_row[i] + priv_row[i + 1]
+                       for i in range(len(priv_row)) if i + 1 < len(priv_row)]
             new_row = [1] + new_row + [1]
             trig.append(new_row)
             priv_row = new_row
