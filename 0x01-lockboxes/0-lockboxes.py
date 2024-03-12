f899acf88ca9e3fe2187ffc0854f3dd4ac463edf
KENC0DE <kenansameseret@gmail.com>
2024-03-12 22:19:40 +0530

    finaly

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+LockBoxes
+"""
+def canUnlockAll(boxes):
+    def opener(boxes, keys, key):
+        if key in keys:
+            return
+        if boxes[key] == []:
+            keys.append(key)
+            return
+        keys.append(key)
+        for i in range(len(boxes[key])):
+            opener(boxes, keys, boxes[key][i])
+
+    keys = [-1,]
+    key = 0
+    opener(boxes, keys, key)
+    keys = keys[1:]
+    return len(boxes) == len(keys)
