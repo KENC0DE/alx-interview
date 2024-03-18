fe2db083960d5eea16f81b662ddaa8141ac19be1
kenc0de <kenansameseret@gmail.com>
2024-03-18 21:10:00 +0530

    continue

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0. Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    calculate and return the number of function
+    needed to reach the required n characters
+    """
+
