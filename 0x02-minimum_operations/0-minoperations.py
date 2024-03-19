1301d333b6f125f8b972bbb9f9633a231b0feb0c
KENC0DE <kenansameseret@gmail.com>
2024-03-19 20:10:47 +0530

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,17 +9,18 @@ def is_prime(n):
         return False
     if n <= 3:
         return True
-    
+
     if n % 2 == 0 or n % 3 == 0:
         return False
-    
+
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return False
         i += 6
-    
+
     return True
+
 
 def minOperations(n):
     """
@@ -48,4 +49,4 @@ def minOperations(n):
             return n // 2 + 1
 
     if n % 2 != 0:
-        return (n - 1) / 2
+        return (n - 1) // 2
