ad8d7f6e3158576ad21fb0451a9421a2de49ca9d
KENC0DE <kenansameseret@gmail.com>
2024-03-19 19:26:14 +0530

    min

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,7 +6,34 @@
 
 def minOperations(n):
     """
-    calculate and return the number of function
-    needed to reach the required n characters
-    """
+    Calculate the minimum number of
+    operations needed to obtain n 'H' characters.
 
+    Args:
+        n (int): The desired number of 'H' characters.
+
+    Returns:
+        int: The minimum number of operations needed,
+        or 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+    if n in [2, 3, 4]:
+        return n
+
+    ops = {'copy': 0, 'paste': 0}
+    chrs = 1  # Start with one 'H' character
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            break
+        else:
+            return n
+
+    if n % 2 == 0:
+        if n % 4 != 0:
+            return n // 2 + 2
+        else:
+            return n // 2 + 1
+
+    # continue her for odd n.
