2cea09b30b2f4ba60f8080f61c894c4626574c03
KENC0DE <kenansameseret@gmail.com>
2024-03-27 03:31:10 +0530

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,37 +5,37 @@ Log parsing
 import sys
 
 
-log = {'ipAddress': [], 'fileSize': 0, 'statusCode': {
+def print_stats(fileSize, statusCode):
+    """Print the currenct stats"""
+    print('File size: {}'.format(fileSize))
+    for key, val in statusCode.items():
+        if val > 0:
+            print('{}: {}'.format(key, val))
+
+
+fileSize = 0
+statusCode = {
     '200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-    '404': 0, '405': 0, '500': 0}
-    }
+    '404': 0, '405': 0, '500': 0
+}
 
 n_input = 0
 
 try:
     for line in sys.stdin:
         m_line = line.strip().split()
-        n_input += 1
         nec = m_line[-2:]
-        if len(m_line) < 5:
-            continue
+        if len(m_line) >= 5:
+            n_input += 1
 
-        stat_code = int(nec[0])
-        file_size = int(nec[1])
-        log['statusCode'][nec[0]] += 1
-        log['fileSize'] += file_size
+            stat_code = int(nec[0])
+            file_size = int(nec[1])
+            statusCode[nec[0]] += 1
+            fileSize += file_size
 
-        if n_input == 10:
-            print('File size: {}'.format(log['fileSize']))
-            codes = log['statusCode']
-            for key, val in codes.items():
-                if val > 0:
-                    print('{}: {}'.format(key, val))
+            if n_input == 10:
+                print_stats(fileSize, statusCode)
 
-            n_input = 0
+                n_input = 0
 finally:
-    print('File size: {}'.format(log['fileSize']))
-    codes = log['statusCode']
-    for key, val in codes.items():
-        if val > 0:
-            print('{}: {}'.format(key, val))
+    print_stats(fileSize, statusCode)
